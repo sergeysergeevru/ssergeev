e9a9ee9abf28fcaf054171e8ff344cb9a15a4ecc
Sergey Sergeev <rocketracoon@MBP-Sergey.Dlink>
2019-01-08 01:48:59 +0300

    added work experience

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './index.scss';
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
 
 var app = new Vue({
     el: '#app',
